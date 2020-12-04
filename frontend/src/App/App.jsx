00d8ae5e9ff14c5ebc98081f0e28926065d8e641
Jesús F. Vázquez Biltre <jesusfvb2@gmail.com>
2020-12-04 14:20:20 -0500

    listo para trabajar con material ui

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -1,41 +1,56 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import logo from "./helpers/img/logo.png";
-import { Button, Col, Container, Form, Image, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
 import { POST, PUT } from "./helpers/_Axion";
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      login: null,
-      userName: "",
-      authorities: [],
-    };
-    this.handleLogin = this.handleLogin.bind(this);
-    this.handleLogout = this.handleLogout.bind(this);
-  }
-  componentDidMount() {
+
+export const Session = React.createContext({
+  userName: "",
+  authorities: [],
+  cb: null,
+});
+
+const App = () => {
+  const [session, setSession] = useState({
+    login: null,
+    userName: "",
+    authorities: [],
+  });
+
+  useEffect(() => {
+    handleScroll();
+    handleLoad();
+  }, []);
+
+  function handleLoad() {
     let jwt = window.localStorage.getItem("jwt");
     if (jwt === null || jwt === undefined) {
-      this.setState({ login: false });
+      setSession({
+        login: false,
+        userName: "",
+        authorities: [],
+      });
       console.error("error al iniciar sesión");
     } else {
       PUT("/login", { jwt: jwt })
         .then((response) => {
           let Jwt = require("jsonwebtoken");
           let data = Jwt.decode(jwt).sub.split(",");
-          this.setState({
+          setSession({
             login: !response,
             userName: data.shift(),
             authorities: data,
           });
         })
-        .catch((error) =>
-          console.error("error al comprobar si estaba autenticado", error)
-        );
+        .catch((error) => {
+          console.error("error al comprobar si estaba autenticado", error);
+          setSession({
+            login: false,
+            userName: "",
+            authorities: [],
+          });
+        });
     }
   }
-  handleLogin(e) {
+
+  function handleLogin(e) {
     e.preventDefault();
     POST("/login", {
       userName: e.target.userName.value,
@@ -45,7 +60,7 @@ class App extends React.Component {
         window.localStorage.setItem("jwt", response.jwt);
         let Jwt = require("jsonwebtoken");
         let data = Jwt.decode(response.jwt).sub.split(",");
-        this.setState({
+        setSession({
           login: true,
           userName: data.shift(),
           authorities: data,
@@ -53,70 +68,32 @@ class App extends React.Component {
       })
       .catch((error) => console.error(error));
   }
-  handleLogout() {
+
+  function handleLogout() {
     window.localStorage.clear();
-    this.setState({ login: false, userName: "", authorities: [] });
+    setSession({
+      login: false,
+      userName: "",
+      authorities: [],
+    });
   }
-  render() {
-    if (this.state.login == null) {
-      return null;
-    } else if (!this.state.login) {
-      document.getElementsByTagName("body")[0].classList.add("colorBodyLogin");
-      return (
-        <Container fluid className="myCenter ">
-          <Row>
-            <Col></Col>
-            <Col
-              xl="3"
-              lg="4"
-              md="5"
-              sm="6"
-              xs="9"
-              className="shadow p-3 mb-5 bg-white rounded border colorLogin"
-            >
-              <Form onSubmit={(e) => this.handleLogin(e)}>
-                <Image src={logo} fluid className="mb-4" />
-                <Form.Group>
-                  <Form.Label>Usuario</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Entre el Usuario"
-                    id="userName"
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>Contraseña</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Entre la Contraseña"
-                    id="password"
-                  />
-                </Form.Group>
-                <Form.Group className="text-center">
-                  <Button variant="secondary" type="submit">
-                    Entrar
-                  </Button>
-                </Form.Group>
-              </Form>
-            </Col>
-            <Col></Col>
-          </Row>
-        </Container>
-      );
-    } else {
-      document
-        .getElementsByTagName("body")[0]
-        .classList.remove("colorBodyLogin");
-      return (
-        <div>
-          <label>{this.state.userName}</label>
-          {this.state.authorities.map((authority, key) => (
-            <label key={key}>_{authority}</label>
-          ))}
-          <button onClick={this.handleLogout}>Logout</button>
-        </div>
-      );
-    }
+
+  function handleScroll() {
+    let scroll = 0;
+    document.addEventListener("scroll", () => {
+      let filtro = document.getElementById("filtrar");
+      if (window.scrollY < scroll && filtro !== null) {
+        scroll = window.scrollY;
+        filtro.classList.remove("d-none");
+      }
+      if (window.scrollY > scroll && filtro !== null) {
+        scroll = window.scrollY;
+        filtro.classList.add("d-none");
+      }
+    });
   }
-}
+
+  return <h1>Jesus</h1>;
+};
+
 export default App;
